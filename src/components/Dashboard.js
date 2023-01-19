16d2792a0f3c975b160c0b7dc4648045755ccef8
Eszter Egyud <eszter.egyud@telus.com>
2023-01-19 16:28:59 -0700

    Add componentDidMount and componentDidUpdate methods

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-
+import Loading from "components/Loading";
+import Panel from "components/Panel";
 import classnames from "classnames";
-import Loading from "./Loading";
-import Panel from "./Panel";
-
 
 const data = [
   {
@@ -28,41 +26,54 @@ const data = [
   }
 ];
 
-
 class Dashboard extends Component {
+
+
   state = {
     loading: false,
     focused: null
   };
 
-  Dashboard(props) {
-    const [state, setState] = React.useState({ focused: null });
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
 
-    function selectPanel(id) {
-      setState({
-        focused: id
-      });
+    if (focused) {
+      this.setState({ focused });
     }
   }
 
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
+
   render() {
-
-
-    const dashboardClasses = classnames("dashboard", { "dashboard--focused": this.state.focused });
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data).map(panel => (
-      <Panel
-        key={panel.id}
-        id={panel.id}
-        label={panel.label}
-        value={panel.value}
-        onSelect={this.selectPanel}
-      />
-    ));
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={event => this.selectPanel(panel.id)}
+        />
+      ));
+
 
     return <main className={dashboardClasses}>{panels}</main>;
   }
